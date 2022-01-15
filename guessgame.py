d881583b7915ee646d5e7985c0dbae2dc2d03038
hassanqari9 <76206623+hassanqari9@users.noreply.github.com>
2022-01-16 00:34:17 +0530

    Create guessgame.py

---
+++ guessgame.py
@@ -0,0 +1,84 @@
+import random 
+print("""
+          _.----.                          
+       .-"       \-.                       
+      /           ; \                      
+     :           /:  \                     
+     ;         .'  ;  ;                    
+     ;      .-"    :  :                    
+    :   _.+(   .-- :  :                    
+    ;  ;   ' :  :                    
+    ;  :           ;  ;                    
+    :   ;    -    :  :                     
+     )  '   .-.   '  :                     
+    (    '. `"' .'   ;                     
+     "-._.:`---':-"-.'+'                   
+          ;     ;    "                     
+   _..__.-. -. (:                          
+ ,'   .:(o);     "-._                      
+ :    _: 0 ;        \`.                    
+ ;  .'/.\-/-.        `:                    
+:  : :  -U--:"-.  \    ;                   
+;  ; :  ----;   "-.L.-" \                  
+'. '  \ ---(      ;O:    ;                 
+  \ '. '-;-'      :-:    :                 
+   `. ""/         ; :    ;                 
+     ""T      .-":  :`. /                  
+       :  --""   :   ; Y                   
+        ;        ;   : :                   
+        :       :     ; ;                  
+         ;      :   ; : :                  
+         :      ;   :  ; \                 
+          ;    :    ;  :  \_               
+          :    :        \  \"-.            
+          ;    ;         \  `. "-.         
+         :    :     c     \   `./"-._      
+         ;    :            \    \    "-.   
+        :     ;             `.   ;-.  -.`. 
+        :    :       __..--"" \  :  `.\.`.\
+        ;    :_..--"";  ;  _.-'\  ;   ")))T
+       :     ;      _L.-'""     ; :    '-='
+       ;    :_..--""            :  ;       
+      /     ;                   ;; :       
+    .'     /                    ;: J       
+    `.    /                     ;'"        
+      :-.'         /\           ;          
+      ;           /  ;          :          
+     :           /   :          :          
+     ;          /     ;         :          
+    :          /      ;         :          
+    ;         /       :         :          
+   :         /        :         :    
+WELCOME TO GUESSING GAME😋
+      """)
+level = input("Choose difficulty level\nEasy or Hard: ").lower()
+
+random_number = random.randint(1,100)
+
+if level == "easy":
+  life = 10
+  print(f"Life:{life}")
+elif level == "hard":
+  life = 5
+  print(f"Life:{life}")
+else:
+  print("Only Choose between Easy or Hard")
+print("Guess a number between 1-100")
+while(life > 0):
+    guess = int(input("Guess the number: "))
+    if guess < random_number:
+      print("Guess higher")
+      life -= 1
+      print(f"Life:{life}")
+      
+    elif guess > random_number:
+      print("Guess lower")
+      life -= 1
+      print(f"Life:{life}")
+      
+    elif guess == random_number:
+      print("Correct guess")
+      life = -1
+
+if life == 0:
+  print("You lost")
